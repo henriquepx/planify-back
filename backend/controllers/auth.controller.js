2a8ce593561aeb22e6c5d79e826af27580f7ba8c
Vinicius Nobrega Costa <viniciusnobcosta@gmail.com>
2024-10-01 19:52:33 -0300

    Create and add OAuth component

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,3 +57,29 @@ export const signin = async (req, res, next) => {
         next(error);
     }
 };
+
+export const google = async (req, res, next) => {
+    try{
+        const user = await User.findOne({ email: req.body.email })
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { user_password: hashedPassword, ...rest } = user._doc;
+            const expiryDate = new Date(Date.now() + 3600000);
+            res.cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        } else {
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8)
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.floor(Math.random * 10000).toString(), email: req.body.email, user_password: hashedPassword});
+            await newUser.save()
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { user_password: hashedPassword2,...rest } = newUser._doc;
+            const expiryDate = new Date(Date.now() + 3600000);
+            res.cookie('access_token', token, {
+                httpOnly:true,
+                expires: expiryDate,
+            });
+        }
+    } catch (error) {
+        next(error)
+    }
+}
